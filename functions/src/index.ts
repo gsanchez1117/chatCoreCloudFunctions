2712c801b3bb284183cd2afbe3a1d37ce792b0d8
MACINTOSH <macintosh@MACINTOSHs-MacBook-Pro-2.local>
2018-10-06 12:16:40 -0700

    added function to create a user profile in firestore when a new user is created.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,31 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//initialise the app for admin use
+admin.initializeApp();
+
+//get an instance of the firestore database
+const db = admin.firestore();
+
+//Help: https://bigcodenerd.org/create-user-profile-firestore-authentication/
+
+/**
+ * Creates a document with ID -> uid in the `Users` collection.
+ *
+ * @param {Object} userRecord Contains the auth, uid and displayName info.
+ * @param {Object} context Details about the event.
+ */
+const createProfile = (userRecord, context) => {
+    return db.collection('Users').doc(userRecord.uid).set({
+      displayName: 'Anonymous',
+      email: userRecord.email,
+    }).catch(console.error);
+  };
+
+/**
+ * functions to deploy
+ */
+module.exports = {
+    //create a user profile when a new user is created
+    authOnCreate: functions.auth.user().onCreate(createProfile),
+};
